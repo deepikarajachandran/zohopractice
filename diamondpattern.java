29b21c7e38046b886dc629dc36d74d4aae3a889d
deepikarajachandran <99521262+deepikarajachandran@users.noreply.github.com>
2022-05-04 15:12:00 +0530

    Update diamondpattern.java

--- diamondpattern.java
+++ diamondpattern.java
@@ -1,5 +1,15 @@
 package com.company;
+/*
 
+        1 
+      3   2 
+    6   5   4 
+  10   9   8   7 
+  10   9   8   7 
+    6   5   4 
+      3   2 
+        1 
+*/
 public class diamondpattern {
     public static void main(String[] args) {
         int k=1,q=2;
